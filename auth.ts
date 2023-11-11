be0d449c4637fdedf94804e69007364e7ca3e59b
mgregerson <mgregerson4321@gmail.com>
2023-11-10 20:34:57 -0500

    added auth options with token in session

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { adminAuth, adminDb } from "./firebase-admin";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
+          // We have access to the firebase admin sdk. We need to pass in the user id and create a custom token. Then, the user will use this custom token to authenticate on firebase on the client side.
+          const firebaseToken = await adminAuth.createCustomToken(token.sub);
+          session.firebaseToken = firebaseToken;
+        }
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  adapter: FirestoreAdapter(adminDb),
+} satisfies NextAuthOptions;
