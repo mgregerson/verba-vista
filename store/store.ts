2baf7b89a8889ee3a809357817607731cafd4d1f
mgregerson <mgregerson4321@gmail.com>
2023-11-14 18:35:06 -0500

    new language store to handle languages supported for free vs pro members

--- store/store.ts
+++ store/store.ts
@@ -30,12 +30,36 @@ export const LanguagesSupportedMap: Record<LanguagesSupported, string> = {
   pl: "Polish",
 };
 
+interface LanguageState {
+  language: LanguagesSupported;
+  setLanguage: (language: LanguagesSupported) => void;
+  getLanguages: (isPro: boolean) => LanguagesSupported[];
+  getNotSupportedLanguages: (isPro: boolean) => LanguagesSupported[];
+}
+
 interface SubscriptionState {
-  subscription: Subscription | null;
+  subscription: Subscription | null | undefined;
   setSubscription: (subscription: Subscription | null) => void;
 }
 
 export const useSubscriptionStore = create<SubscriptionState>((set) => ({
-  subscription: null,
+  subscription: undefined,
   setSubscription: (subscription: Subscription | null) => set({ subscription }),
+}));
+
+export const useLanguageStore = create<LanguageState>((set) => ({
+  language: "en",
+  setLanguage: (language: LanguagesSupported) => set({ language }),
+  getLanguages: (isPro: boolean) => {
+    if (isPro)
+      return Object.keys(LanguagesSupportedMap) as LanguagesSupported[];
+    return Object.keys(LanguagesSupportedMap).slice(
+      0,
+      8
+    ) as LanguagesSupported[];
+  },
+  getNotSupportedLanguages: (isPro: boolean) => {
+    if (isPro) return [];
+    return Object.keys(LanguagesSupportedMap).slice(8) as LanguagesSupported[];
+  },
 }));
