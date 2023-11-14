440c7f6ff241bc38ceb571c4ad7e1c02760df1dd
mgregerson <mgregerson4321@gmail.com>
2023-11-14 18:33:02 -0500

    New style changes for Checkout Button

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -4,11 +4,19 @@ import { db } from "@/firebase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import ManageAccountButton from "./ManageAccountButton";
 
 function CheckoutButton() {
   const { data: session } = useSession();
-
   const [loading, setLoading] = useState(false);
+  const subscription = useSubscriptionStore((state) => state.subscription);
+
+  const isLoadingSubscription = subscription === undefined;
+  const isSubscribed =
+    subscription?.status === "active" && subscription?.role === "pro";
+
   const createCheckoutSession = async () => {
     if (!session?.user.id) return;
 
@@ -42,14 +50,25 @@ function CheckoutButton() {
   };
 
   return (
-    <div>
-      {/* If user is subscribed show me the user is subscribed */}
-      <button
-        onClick={() => createCheckoutSession()}
-        className="w-full max-w-md mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus:visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
-      >
-        {loading ? "loading..." : "Sign Up"}
-      </button>
+    <div className="flex flex-col space-y-2">
+      {isSubscribed && (
+        <>
+          <hr className="mt-5" />
+          <p className="pt-5 text-center text-cs text-indigo-600">
+            You are subscribed to the Pro Plan!
+          </p>
+        </>
+      )}
+
+      <div className="w-full max-w-md mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus:visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
+        {isSubscribed ? (
+          <ManageAccountButton />
+        ) : isLoadingSubscription || loading ? (
+          <LoadingSpinner />
+        ) : (
+          <button onClick={() => createCheckoutSession()}>Sign Up</button>
+        )}
+      </div>
     </div>
   );
 }
