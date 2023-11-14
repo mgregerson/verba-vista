8ee9cede094b0fc72afaced277db797e1446bf41
mgregerson <mgregerson4321@gmail.com>
2023-11-14 18:29:23 -0500

    new component to delete a chat

---
+++ components/DeleteChatButton.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { useToast } from "./ui/use-toast";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useAdminId from "@/hooks/useAdminId";
+
+function DeleteChatButton({ chatId }: { chatId: string }) {
+  const { data: session } = useSession();
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const adminId = useAdminId({ chatId });
+
+  async function handleDelete() {
+    toast({
+      title: "Deleting chat",
+      description: "Please wait while we delete the chat...",
+    });
+
+    console.log("Deleting chat: ", chatId);
+
+    await fetch("/api/chat/delete", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ chatId: chatId }),
+    })
+      .then((res) => {
+        toast({
+          title: "Success",
+          description: "Youre chat has been deleted.",
+          className: "bg-green-600 text-white text-center",
+          duration: 3000,
+        });
+        router.replace("/chat");
+      })
+      .catch((err) => {
+        toast({
+          title: "Error",
+          description: "There was an error deleting your chat :(",
+          variant: "destructive",
+        });
+      })
+      .finally(() => setOpen(false));
+  }
+
+  return (
+    session?.user.id === adminId && (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="destructive">Delete Chat</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogDescription>
+              This will delete the chat for all users.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid grid-cols-2 space-x-2">
+            <Button variant={"destructive"} onClick={handleDelete}>
+              Delete
+            </Button>
+            <Button variant={"outline"} onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    )
+  );
+}
+
+export default DeleteChatButton;
